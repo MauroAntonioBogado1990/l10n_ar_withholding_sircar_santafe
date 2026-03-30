af1b37ae090bd9b0a76360190cae785099a09e17
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2026-03-30 16:41:12 -0300

    add change in function report xlsx

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,9 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import models
+# -*- coding: utf-8 -*-
+from . import account_export_santafe
+from . import padron_santafe
+from . import import_padron_santafe
+from . import res_partner_inherit
+from . import account_payment_group_inherit
+from . import account_move_inherit
+from . import account_tax_inherit
+from . import res_company

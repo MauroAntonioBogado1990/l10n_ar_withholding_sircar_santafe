a12b27f558a60eb4e9c724a0bd31894036a0986e
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2026-04-01 12:15:54 -0300

    update from module

--- models/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_export_santafe
-from . import padron_santafe
-from . import import_padron_santafe
-from . import res_partner_inherit
-from . import account_payment_group_inherit
-from . import account_move_inherit
-from . import account_tax_inherit
-from . import res_company

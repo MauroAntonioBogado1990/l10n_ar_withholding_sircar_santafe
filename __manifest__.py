add956a0b77eec681e048a6b4fa31b5a19818fea
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2026-04-09 15:35:30 -0300

    add new values

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'author': 'ADHOC SA, Moldeo Interactive, Exemax, Codize',
     'category': 'Accounting & Finance',
     'data': [
-        #'views/account_export_santafe_view.xml',
+        'views/account_export_santafe_view.xml',
         'views/santafe_padron.xml',
         'views/import_padron_santafe_view.xml',
         #'views/res_partner_view.xml',
@@ -18,5 +18,5 @@
         'l10n_ar_sircar_regimenes',
     ],
     'installable': True,
-    'version': '18.0.1.0.0',
+    'version': '16.0.1.0.0',
 }
